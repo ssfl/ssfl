1d36c9adc774829c3a487d25d5bbf4d0efd27aef
datvo06 <datvo06@gmail.com>
2024-10-09 10:47:08 +1100

    Minor

--- codeflaws/dataloader_gumtree.py
+++ codeflaws/dataloader_gumtree.py
@@ -2,12 +2,19 @@ from dgl.data import DGLDataset
 from dgl import save_graphs, load_graphs
 import dgl
 from utils.utils import ConfigClass
-from codeflaws.data_utils import all_codeflaws_keys,\
-    get_nx_ast_node_annt_gumtree, get_nx_ast_stmt_annt_gumtree
+from codeflaws.data_utils import (
+    all_codeflaws_keys,
+    get_nx_ast_node_annt_gumtree,
+    get_nx_ast_stmt_annt_gumtree,
+)
 from utils.gumtree_utils import GumtreeASTUtils
 from utils.data_utils import AstNxDataset, AstGraphMetadata
-from graph_algos.nx_shortcuts import nodes_where, edges_where,\
-        where_node, where_node_not
+from graph_algos.nx_shortcuts import (
+    nodes_where,
+    edges_where,
+    where_node,
+    where_node_not,
+)
 from collections import defaultdict
 import os
 import random
@@ -18,25 +25,34 @@ import tqdm
 
 from json import JSONDecodeError
 
+
 class CodeflawsGumtreeNxStatementDataset(AstNxDataset):
     def __init__(self, save_dir=ConfigClass.preprocess_dir_codeflaws):
         super().__init__(
-                all_entries=all_codeflaws_keys,
-                process_func=get_nx_ast_stmt_annt_gumtree,
-                save_dir=save_dir,
-                name='gumtree_stmt',
-                special_attrs=[('stmt_nodes', 
+            all_entries=all_codeflaws_keys,
+            process_func=get_nx_ast_stmt_annt_gumtree,
+            save_dir=save_dir,
+            name="gumtree_stmt",
+            special_attrs=[
+                (
+                    "stmt_nodes",
                     lambda nx_g: nodes_where(
-            nx_g, lambda x: GumtreeASTUtils.check_is_stmt_cpp(nx_g.nodes[x]['ntype'], graph='ast')
-            ))])
+                        nx_g,
+                        lambda x: GumtreeASTUtils.check_is_stmt_cpp(
+                            nx_g.nodes[x]["ntype"], graph="ast"
+                        ),
+                    ),
+                )
+            ],
+        )
 
 
 class CodeflawsGumtreeNxNodeDataset(AstNxDataset):
-    def __init__(self,
-                 save_dir=ConfigClass.preprocess_dir_codeflaws):
+    def __init__(self, save_dir=ConfigClass.preprocess_dir_codeflaws):
         super().__init__(
-                all_entries=all_codeflaws_keys,
-                process_func=get_nx_ast_node_annt_gumtree,
-                save_dir=save_dir,
-                name='gumtree_node',
-                special_attrs=[])
+            all_entries=all_codeflaws_keys,
+            process_func=get_nx_ast_node_annt_gumtree,
+            save_dir=save_dir,
+            name="gumtree_node",
+            special_attrs=[],
+        )
